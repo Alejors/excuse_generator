72d8a035a1e558bd5411bb842b5343906d0a2b1d
Alejandro <alejoatria@gmail.com>
2022-06-16 19:36:11 +0000

    Proyecto con 4 alternativas para cada elemento

--- src/app.js
+++ src/app.js
@@ -4,8 +4,29 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+let elementos = {
+  quien: ["mi perro", "mi abuela", "Thor", "Piolín"],
+  accion: ["se comió", "se llevó", "vendió", "perdió"],
+  que: ["mi tarea", "mis llaves", "mi auto", "mi bicicleta"],
+  cuando: [
+    "ayer.",
+    "justo cuando iba saliendo.",
+    "a la hora de almuerzo.",
+    "durante mi siesta."
+  ]
+};
+let excusa = () => {
+  let mentira =
+    elementos.quien[Math.floor(Math.random() * 4)] +
+    " " +
+    elementos.accion[Math.floor(Math.random() * 4)] +
+    " " +
+    elementos.que[Math.floor(Math.random() * 4)] +
+    " " +
+    elementos.cuando[Math.floor(Math.random() * 4)];
 
+  return mentira;
+};
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#excusa").innerHTML = excusa();
 };
